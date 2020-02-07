997810b3aaa35a5175f3c3b5a1ceb086a4de1b15
seunghyun <seunghyunleekr@gmail.com>
2020-02-07 17:32:08 +0900

    .

---
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -0,0 +1,22 @@
+import { generateToken } from "../../../utils";
+import { prisma } from "../../../../generated/prisma-client"
+
+export default {
+    Mutation: {
+        confirmSecret: async (_, args) => {
+            const { email, secret } = args;
+            const user = await prisma.user({ email });
+            if (user.loginSecret === secret) {
+                await prisma.updateUser({
+                    where: { id: user.id },
+                    data: {
+                        loginSecret: ""
+                    }
+                });
+                return generateToken(user.id);
+            } else {
+                throw Error("Wrong email/secret combination");
+            }
+        }
+    }
+}
